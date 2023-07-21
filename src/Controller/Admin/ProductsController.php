39d138ff77e53af03c551a7518d94abc2b6b23ab
Carolina Vargas <carolinaqvargas@gmail.com>
2023-07-21 12:33:26 +0200

    create productForm

--- src/Controller/Admin/ProductsController.php
+++ src/Controller/Admin/ProductsController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller\Admin;
 
+use App\Form\ProductsFormType;
 use App\Entity\Products;
 use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-
-#[Route('/admin/produits', name: 'admin_products_')]
+#[Route('/admin/products', name: 'admin_products_')]
 class ProductsController extends AbstractController
 {
     #[Route('/', name: 'index')]
@@ -24,22 +24,31 @@ class ProductsController extends AbstractController
     {
         //L'ajout est possible uniquement pour les ROLE_ADMIN
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin/products/index.html.twig');
+
+        //On crée un "nouveau produit"
+        $product = new Products();
+
+        // On crée le formulaire
+        $productForm = $this->createForm(ProductsFormType::class, $product);
+
+        return $this->render('admin/products/add.html.twig', [
+            'productForm' => $productForm,
+        ]);
     }
+
+
     #[Route('/edition/{id}', name: 'edit')]
     public function edit(Products $product): Response
     {
         // On vérifie si l'utilisateur peut éditer avec le Voter
         $this->denyAccessUnlessGranted('PRODUCT_EDIT', $product);
 
-        return $this->render('admin/products/index.html.twig');
+        return $this->render('admin/products/edition.html.twig');
     }
 
     #[Route('/suppression/{id}', name: 'delete')]
     public function delete(Products $product): Response
     {
-        return $this->render('admin/products/index.html.twig');
+        return $this->render('admin/products/suppression.html.twig');
     }
 }
-
- 
---
+++ src/Form/ProductsFormType.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Categories;
+use App\Entity\Products;
+use App\Repository\CategoriesRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Validator\Constraints\Positive;
+
+class ProductsFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', options: [
+                'label' => 'Nom'
+            ])
+            ->add('description')
+            ->add('price', MoneyType::class, options: [
+                'label' => 'Prix',
+                'divisor' => 100,
+                'constraints' => [
+                    new Positive(
+                        message: 'Le prix ne peut être négatif'
+                    )
+                ]
+            ])
+            ->add('stock', options: [
+                'label' => 'Unités en stock'
+            ])
+            //     ->add('categories', EntityType::class, [
+            //         'class' => Categories::class,
+            //         'choice_label' => 'name',
+            //         'label' => 'Catégorie',
+            //         'group_by' => 'parent.name',
+            //         'query_builder' => function(CategoriesRepository $cr){
+            //             return $cr->createQueryBuilder('c')
+            //                 ->where('c.parent IS NOT NULL')
+            //                 ->orderBy('c.name', 'ASC');
+            //         }
+            //     ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Products::class,
+        ]);
+    }
+}
